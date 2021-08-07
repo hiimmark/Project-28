587d5c6c3d986bd824e6c040c4f76b1ecf902572
hiimmark <76633497+hiimmark@users.noreply.github.com>
2021-08-07 19:30:49 -0400

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,29 @@
+class launcher{
+    constructor(bodyA1, pointB1){
+        var options ={
+            bodyA : bodyA1,
+            pointB: pointB1, 
+            stiffness:0.04,
+            length:10
+        }
+        this.pointB = pointB1;
+        this.bodyA = bodyA1;
+        this.sling = Constraint.create(options);
+
+        World.add(world, this.sling);
+    }
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+}
